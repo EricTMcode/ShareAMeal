617820125f1a926fc74fafde2f2e8f9de08f6bba
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-01-10 15:01:06 +0100

    Initial Commit

---
+++ ShareAMeal/ShareAMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareAMealApp.swift
+//  ShareAMeal
+//
+//  Created by Eric on 10/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct ShareAMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
