de7c272610ae1a4c28a9108e3a7d3b2538b6ae65
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-01-13 14:57:16 +0100

    Add Location struct to Foodbank

--- ShareAMeal/Foodbank.swift
+++ ShareAMeal/Foodbank.swift
@@ -5,6 +5,7 @@
 //  Created by Eric on 10/01/2025.
 //
 
+import CoreLocation
 import Foundation
 
 struct Foodbank: Codable, Identifiable {
@@ -43,5 +44,24 @@ extension Foodbank {
         var excess: String?
 
         static let example = Items(id: "Example", needs: "Example Needed Item", excess: "Example Excess Item")
+    }
+}
+
+struct Location: Codable, Identifiable {
+    enum CodingKeys: String, CodingKey {
+        case name, slug, address, location = "lat_lng"
+    }
+
+    var id: String { slug }
+    var name: String
+    var slug: String
+    var address: String
+    var location: String
+
+    var coodinate: CLLocationCoordinate2D? {
+        let components = location.split(separator: ",").compactMap(Double.init)
+        guard components.count == 2 else { return nil }
+
+        return CLLocationCoordinate2D(latitude: components[0], longitude: components[1])
     }
 }
