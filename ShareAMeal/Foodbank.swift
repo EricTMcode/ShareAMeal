3f23ea047bfd9b3faad4340d4a873e5c15a28e5c
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-01-13 14:06:47 +0100

    Create Foodbank extension

--- ShareAMeal/Foodbank.swift
+++ ShareAMeal/Foodbank.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct Foodbank: Codable, Identifiable {
     enum CodingKeys: String, CodingKey {
-        case name, slug, phone, email, address, distance = "distance_m"
+        case name, slug, phone, email, address, distance = "distance_m", items
     }
 
     var id: String { slug }
@@ -19,6 +19,7 @@ struct Foodbank: Codable, Identifiable {
     var email: String
     var address: String
     var distance: Int
+    var items: Items
 
     var distanceFormatted: String {
         let measurement = Measurement(value: Double(distance), unit: UnitLength.meters)
@@ -26,6 +27,14 @@ struct Foodbank: Codable, Identifiable {
         let measurementString = measurement.formatted(.measurement(width: .wide))
         return "\(measurementString) from you"
     }
+}
 
+extension Foodbank {
+    struct Items: Codable, Identifiable {
+        var id: String
+        var needs: String
+        var excess: String?
 
+        static let example = Items(id: "Example", needs: "Example Needed Item", excess: "Example Excess Item")
+    }
 }
